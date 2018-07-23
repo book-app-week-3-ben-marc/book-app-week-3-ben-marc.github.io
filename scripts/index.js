666d9e81a4ab176de1ffa0e490fb112603170c70
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-07-23 16:11:25 -0500

    Setup module file

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var app = app || {};
+
+(function (module) {
+  let productionApiUrl = 'https://bv-mh-booklist.herokuapp.com';
+  let developmentApiUrl = 'http://localhost:3000';
+
+  module.isProduction = /^(?!localhost|127)/.test(window.location.hostname);
+
+  module.ENVIRONMENT = {
+    apiUrl: module.isProduction ? productionApiUrl : developmentApiUrl,
+  };
+
+  console.log(module.ENVIRONMENT);
+
+  $.getJSON(module.ENVIRONMENT.apiUrl + '/tasks')
+    .then(result => console.log(result))
+    .catch(err => console.error(err));
+})(app);
