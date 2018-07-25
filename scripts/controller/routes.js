e56f58da07e8f49fff3301384e0fe612b7bc9ace
Keith Dahlby <keith@newbo.co>
2018-07-25 14:13:57 -0500

    Relocate /books/add route ahead of /books/:id

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -2,9 +2,12 @@
 'use-strict';
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
+
+// This MUST come before /books/:id otherwise :id is set to 'add'
+page('/books/add', ctx => app.bookView.initFormPage(ctx));
+
 page('/books/:id', ctx => {
   app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage);
 });
-page('/api/v1/books/add', ctx => app.bookView.initFormPage(ctx));
 
 page();
