ed467514bc9f267c998c9833830b0cdd91e70153
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-07-24 22:40:36 -0500

    Attempted form page

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -2,9 +2,9 @@
 'use-strict';
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
-page('/books/:id', ctx =>{
+page('/books/:id', ctx => {
   app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage);
 });
-
+page('/api/v1/books/add', ctx => app.bookView.initFormPage(ctx));
 
 page();
